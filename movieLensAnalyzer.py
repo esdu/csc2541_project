ebdad194c4b55d523d01d538da9e2e657903415c
Soon Chee Loong <scheeloong@gmail.com>
2017-11-10 12:58:15 -0500

    Added todo and refactored

--- movieLensAnalyzer.py
+++ movieLensAnalyzer.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import os # For reading files 
+import random
 
 # import panda as pd , TODO: Later after fixing Ubuntu
 
@@ -20,6 +21,27 @@ class MovieLensAnalyzer(object):
         self.userFile = os.path.join(self.dataDirectory, 'u.user')
         self.userPreferences= self.parseUserPreference()
         self.userMovieRatingMatrix = self.parseUserMovieMatrix()
+	self.mask = None
+
+    def simplifyMatrix(self, numElements):
+        startIndex = 50
+        endIndex = startIndex + int(numElements)
+        self.userMovieRatingMatrix = self.userMovieRatingMatrix[startIndex:endIndex, startIndex:endIndex]
+
+    def generateMask(self, percentageZero):
+        '''
+        Generates the mask for the userMovieRatingMatrix
+        '''
+        self.mask = np.ones(self.userMovieRatingMatrix.shape)
+        for i in range(self.mask.shape[0]):
+	    for j in range(self.mask.shape[1]):
+		if (random.random() < float(percentageZero)):
+		    self.mask[i][j] = 0
+
+    def getMaskedUserMovieRatingMatrix(self):
+        if self.mask is None:
+            raise Exception("Masked not initialize using generateMask()")
+        return self.userMovieRatingMatrix * self.mask
 
     def getUserMovieRatingMatrix(self):
         return self.userMovieRatingMatrix
@@ -35,6 +57,15 @@ class MovieLensAnalyzer(object):
         movies_df['MovieID'] = movies_df['MovieID'].apply(pd.to_numeric)
         '''
 
+    # Evaluation: rmse
+    def rootMeanSquareError(self, reconstructionMatrix):
+	return np.sqrt(np.mean((reconstructionMatrix - self.userMovieRatingMatrix)**2))
+
+    # Evaluation: meanAveragePrecision
+    def meanAveragePrecision(self, reconstructionMatrix):
+        # TODO: Calculate MAP
+        return 0
+
     def parseUserPreference(self):
         userPreferences = {} # Index each user using the ID as key
         with open(self.userFile) as userFile:
@@ -46,6 +77,16 @@ class MovieLensAnalyzer(object):
         return userPreferences
 
     def parseUserMovieMatrix(self):
+        # TODO: Sort the user by timestamps for dividing train and test splits
+        # Step1: Sort all the ratings by timestamps
+        # Step2: Create a train matrix from earlier parts of the timestamps
+        # Step3: Create a test matrix from later parts of the timestamps
+        # TODO: Handle case where a user only exist in train or test. 
+        # FixIdea1: Just get intersection of users in train and test and delete those who arent in any of 2.
+        # Step4: Combine both test and train using or operation to get final userRatingMatrix
+        # Step5: Test matrix tells you where you are allowed to explore
+        
+        # with open(self.dataFile) as dataFile:
         with open(self.dataFile) as dataFile:
             # Get all the user and movie ratings into ids
             ratingMatrix = np.zeros((943, 1682)) # 943 users from 1 to 943, 1682 items based on dataset
@@ -97,7 +138,11 @@ class MovieLensRating(object):
 
 if __name__ == "__main__": 
     movieLensAnalyzer = MovieLensAnalyzer()
+    movieLensAnalyzer.simplifyMatrix(20)
     # Returns as a numpy array
     userMovieRatingMatrix = movieLensAnalyzer.getUserMovieRatingMatrix()
+    movieLensAnalyzer.generateMask(0.3)
+    haha = 0.3
+    movieLensAnalyzer.generateMask(haha)
     print(userMovieRatingMatrix.shape)
 
