c728ba558d231a8dcfd5afd073b655c07ef7bb79
Franco Lin <francolin@Francos-MacBook-Pro.local>
2017-11-16 03:58:20 -0500

    added demo, and thompson sampling

--- bandit_algorithms.py
+++ bandit_algorithms.py
@@ -14,27 +14,59 @@ class BanditAlgorithms:
         self.model = model
         self.retrain = retrain
 
-    def get_egreedy_score(self, epsilon):
+
+    def initialize_values(self, egreedy, ucb, thompson):
+    	# initialize the user ratings
+    	# egreedy: MAP, ucb: mean + std, thompson: sample
+
+        if egreedy == True:
+            user_ratings = np.mean(self.model.rhat_samples(),axis=0)[self.user]
+            user_indices = np.array(range(len(user_ratings)))
+
+        elif ucb == True:
+            user_mean = np.mean(self.model.rhat_samples(),axis=0)[self.user]
+            user_dev = np.std(self.model.rhat_samples(),axis=0)[self.user]
+
+            user_ratings = user_mean + user_dev
+            user_indices = np.array(range(len(user_ratings)))
+
+        elif thompson == True:
+            #TODO: take a sample from posterior directly instead of taking a sample of the 100 samples..
+            random_sample = random.randint(0,99)
+            user_ratings = self.model.rhat_samples()[random_sample][self.user]
+            user_indices = np.array(range(len(user_ratings)))
+
+        return user_ratings, user_indices
+
+
+
+    def get_score(self, epsilon, egreedy, ucb, thompson):
         # TODO: retraining is prohibitively expensive here...
         
         my_mask = make_bandits_mask(self.mask, self.R) # Note: This returns a copy of mask.
+        
         discount = 1
         score = 0
         
         n_iter = np.sum(my_mask[self.user] == 0)
         
         for _ in range(n_iter):
-            user_ratings = self.model.posterior_map[self.user]
-            user_indices = np.array(range(len(user_ratings)))
+            user_ratings, user_indices = self.initialize_values(egreedy, ucb, thompson)
+
             user_ratings = user_ratings[my_mask[self.user] == 0]
             user_indices = user_indices[my_mask[self.user] == 0]
+            
+            if egreedy == True:
+                if random.random() < epsilon:
+                    # Choose greedy action
+                    idx = np.argmax(user_ratings)
+                else:
+                    # Choose random action
+                    idx = random.randint(0,len(user_ratings)-1)
 
-            if random.random() < epsilon:
-                # Choose greedy action
+            elif ucb == True or thompson == True:
                 idx = np.argmax(user_ratings)
-            else:
-                # Choose random action
-                idx = random.randint(0,len(user_ratings)-1)
+            
 
             my_mask[self.user,user_indices[idx]] = 1
             score += self.R[self.user,user_indices[idx]] * discount
@@ -45,36 +77,8 @@ class BanditAlgorithms:
         
         return score
 
-
-
-    def get_ucb_score(self):
-        # TODO: retraining is prohibitively expensive here...
-        
-        my_mask = make_bandits_mask(self.mask, self.R) # Note: This returns a copy of mask.
-        discount = 1
-        score = 0
-        
-        n_iter = np.sum(my_mask[self.user] == 0)
-        
-        for i in range(n_iter):
-            user_ratings = self.model.posterior_map[self.user]
-            user_dev = np.std(self.model.posterior[self.user], axis=0)
-            user_indices = np.array(range(len(user_ratings)))
-            user_ratings = user_ratings[my_mask[self.user] == 0]
-            user_dev = user_dev[my_mask[self.user] == 0]
-            user_indices = user_indices[my_mask[self.user] == 0]
-            
-            idx = np.argmax(user_ratings + user_dev)
-            
-            my_mask[self.user,user_indices[idx]] = 1
-            score += self.R[self.user,user_indices[idx]] * discount
-            discount *= self.gamma   
-
-            if self.retrain:
-                self.model.train(R=self.R, mask=my_mask, n_iter=5000)
-                
-        return score
-
+    
+    
     def get_best_score(self):
         '''
         user: user index
