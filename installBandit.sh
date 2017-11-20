b2e822397a8404d95278ee70cf5f60f33616b648
Soon Chee Loong <scheeloong@gmail.com>
2017-11-20 18:29:34 -0500

    Added bandit code

---
+++ installBandit.sh
@@ -0,0 +1,5 @@
+# This file collects the bandit code and puts it here
+rm -r RecommendationSystems # Remove any existing stuff
+git clone https://github.com/scheeloong/RecommendationSystems
+cd RecommendationSystems
+bash install.sh
