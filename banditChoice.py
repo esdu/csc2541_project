9d72f16b39ec6f42c7ee7e82f51128cf4f37ab4f
Soon Chee Loong <scheeloong@gmail.com>
2017-11-20 21:26:17 -0500

    Confirmed API with Franco and Soon

--- banditChoice.py
+++ banditChoice.py
@@ -2,6 +2,8 @@ class BanditChoice(object):
     def __init__(self):
         pass
 
-    def evaluate(self, posteriorMatrix, legalMoveMatrix):
-        # TODO: TEMP
-        return 0, 0
+    def evaluate(self, posteriorMatrix, legalItemVector):
+        # posteriorMatrix = (numSamples, numItems)
+        # legalItemVector = (1, numItems)
+        # itemIndex an integer within [0, numItems - 1]
+        return itemIndex
