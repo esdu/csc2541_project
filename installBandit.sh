662b8b4985388d76add0b577d1437ed611ea91cd
Soon Chee Loong <scheeloong@gmail.com>
2017-11-20 22:14:47 -0500

    updated to work with franco's computer

--- installBandit.sh
+++ installBandit.sh
@@ -4,9 +4,28 @@ pushd ~
 rm -rf RecommendationSystems # Remove any existing stuff
 git clone https://github.com/scheeloong/RecommendationSystems
 cd RecommendationSystems
-bash install.sh
+
+# Installation instruction for others
+sudo pip install -r requirements.txt
+sudo pip install . 
+sudo pip install . --upgrade
+
+# For SCL's computer
+#sudo pip3 install -r requirements.txt
+#sudo pip3 install . 
+#sudo pip3 install . --upgrade
+
+# Execute api example
+python main.py
+# python3 main.py, for SCL's computer
+#-------------------------------
+# Remove temporary files
+find . -name '*.pyc' -delete # Python2
+py3clean .  # Python3
+
 cd ..
 # rm -rf RecommendationSystems # Remove any existing stuff, Not sure if able to do this
 popd 
 
-python3 banditApiTest.py
+python banditApiTest.py
+# python3 banditApiTest.py, for SCL's computer
