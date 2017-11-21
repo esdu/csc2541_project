71c3290af29f6cef44cbe9a364bc0b47816f2df5
Eddie <esdu@users.noreply.github.com>
2017-11-20 19:02:42 -0500

    Agreed on API with Soon

--- uncertaintyModel.py
+++ uncertaintyModel.py
@@ -1,19 +1,25 @@
 class UncertaintyModel(object):
     def __init__(self, ratingMatrix):
         self.ratingMatrix = ratingMatrix
-        self.posteriorMatrix = None
+        #self.posteriorMatrix = None
 
-    def reset(self):
-        # Reset the weights as if no training was done
-        # TODO: 
+    def reset(self, seed=None):
+        """
+        Reset the weights as if no training was done.
+        Reset seed.
+        """
+        pass
+    
+    def save(self, fname):
+        return fname
+    
+    def load(self, fname):
+        return True
+    
+    def train(self, legalTrainIndices):
+        # Train the weights based on current legalTrainIndices matrix
         pass
 
-    def setRatingMatrix(self, ratingMatrix):
-        self.ratingMatrix = ratingMatrix
-
-    def getPosteriorMatrix(self):
-        return self.posteriorMatrix
-
-    def train(self, legalTrainIndices):
-        # TODO: train the weights based on current legalTrainIndices
-        return self.posteriorMatrix
+    def sample_for_user(self, i):
+        # return (k, m) matrix of k samples for user i
+        return samples
