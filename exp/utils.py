97609ff8011fa10fc6c18b8e3aa4f337143d5cc2
Eddie Du <sj.eddie@gmail.com>
2017-12-08 21:32:12 -0500

    new matrix to try on

--- exp/utils.py
+++ exp/utils.py
@@ -11,32 +11,50 @@ def load_R():
     return R
 
 
-def desparsify(R, MIN_PERC_FILLED=0.3):
+def _desparsify(R, MIN_PERC_FILLED_ITEMS, MIN_PERC_FILLED_USERS):
     print("Before desparsify: % of items: ", np.sum(R > 0) / (R.shape[0] * R.shape[1]))
 
     R_dense = np.copy(R)
 
     idx = []
-# Remove items
+    # Remove items
     for j in range(R_dense.shape[1]):
         perc_filled = np.sum(R_dense[:,j] > 0) / R_dense.shape[0]
-        if perc_filled >= MIN_PERC_FILLED:
+        if perc_filled >= MIN_PERC_FILLED_ITEMS:
             idx.append(j)
     R_dense = R_dense[:, idx]
     R_dense.shape
 
     idx = []
-# Remove users
+    # Remove users
     for i in range(R_dense.shape[0]):
         perc_filled = np.sum(R_dense[i,:] > 0) / R_dense.shape[1]
-        if perc_filled >= MIN_PERC_FILLED:
+        if perc_filled >= MIN_PERC_FILLED_USERS:
             idx.append(i)
     R_dense = R_dense[idx, :]
     R_dense.shape
 
     print("After desparsify: % of items: ", np.sum(R_dense > 0) / (R_dense.shape[0] * R_dense.shape[1]))
-
+    print("Size: ", R_dense.shape)
     return R_dense
+
+
+def desparsify(R):
+    '''
+    Before desparsify: % of items:  0.0630466936422
+    After desparsify: % of items:  0.555527743012
+    Size:  (510, 47)
+    '''
+    return _desparsify(R, MIN_PERC_FILLED_ITEMS=0.3, MIN_PERC_FILLED_USERS=0.3)
+
+
+def desparsify_2(R):
+    '''
+    Before desparsify: % of items:  0.0630466936422
+    After desparsify: % of items:  0.373593572446
+    Size:  (354, 353)
+    '''
+    return _desparsify(R, MIN_PERC_FILLED_ITEMS=0.1, MIN_PERC_FILLED_USERS=0.2)
 
 
 def prepare_test_users(R, NUM_USERS_DENSE = 20, NUM_USERS_SPARS = 20, PERC_DROP = 0.3):
